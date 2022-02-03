e70dae4997b676cecfa70118a7a833cb161b90a1
Khaled-mostafa509 <78059311+Khaled-mostafa509@users.noreply.github.com>
2022-02-03 14:44:09 +0200

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -29,10 +29,10 @@ urlpatterns = [
     path('users/',include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
     
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
